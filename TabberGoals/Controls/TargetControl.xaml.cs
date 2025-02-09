a007b249f184a885d150a9b053720cec97c6d452
Jayden-Wilson-01 <Jayden@tabber.co.uk>
2025-02-09 16:34:34 +0000

    Target control

---
+++ TabberGoals/Controls/TargetControl.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabberGoals.Controls
+{
+    /// <summary>
+    /// Interaction logic for TargetControl.xaml
+    /// </summary>
+    public partial class TargetControl : UserControl
+    {
+        #region Properties
+        /// <summary>
+        /// The title of the target
+        /// </summary>
+        public string? Title { get; private set; }
+
+        /// <summary>
+        /// The description of target
+        /// </summary>
+        public string? Descripion {  get; private set; }
+
+        /// <summary>
+        /// The progress of the target 
+        /// </summary>
+        public int TimesCompleted { get; private set; }
+
+        /// <summary>
+        /// is target being done today
+        /// </summary>
+        public byte Today { get; private set; }
+
+        /// <summary>
+        /// The ID of the parent goal
+        /// </summary>
+        public int GoalID { get; private set; }
+        #endregion
+
+        public TargetControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Set title property
+        /// </summary>
+        /// <param name="title">The title of the target</param>
+        public void SetTitle(string title)
+        {
+            try
+            {
+                //If title is null then mnake it default
+                if(string.IsNullOrEmpty(title))
+                {
+                    title = "Target Title";
+                }
+
+                //Set value of title property
+                Title = title;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Set description property
+        /// </summary>
+        /// <param name="description">The description of the target</param>
+        public void SetDescription(string description)
+        {
+            try
+            {
+                //If description is null then mnake it default
+                if (string.IsNullOrEmpty(description))
+                {
+                    description = "Target Title";
+                }
+
+                //Set value of description property
+                Descripion = description;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Set times completed property
+        /// </summary>
+        /// <param name="timesCompleted">The status of the target</param>
+        public void SetTimesCompleted(int timesCompleted)
+        {
+            try
+            {
+                //Set value of times completed property
+                TimesCompleted = timesCompleted;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Set today property
+        /// </summary>
+        /// <param name="today">If target is being done today</param>
+        public void SetToday(byte today)
+        {
+            try
+            {
+                //Set value of today property
+                Today = today;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Set goalId property
+        /// </summary>
+        /// <param name="goalID">The ID of the parent goay</param>
+        public void SetGoalID(int goalID)
+        {
+            try
+            {
+                //Set value of goalId property
+                GoalID = goalID;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
---
+++ TabberGoals/Database/DataLogic/TargetsDataLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TabberGoals.TabberUI.Controls;
+using TabberGoals.Controls;
+using TabberGoals.Database.DataAccess;
+
+namespace TabberGoals.Database.DataLogic
+{
+    public class TargetsDataLogic
+    {
+        TargetsDataAccess TargetsDataAccess = new TargetsDataAccess();
+
+        /// <summary>
+        /// Add target to the database and target grid
+        /// </summary>
+        public void AddGoal(string title, string description, int timesCompleted, byte targetToday, int goalId, TabberWrapPanel targetArea)
+        {
+            try
+            {
+                //Add data to database
+                TargetsDataAccess.AddTarget(title, description, timesCompleted, targetToday, goalId);
+
+                //Create target and add details
+                TargetControl targetControl = new TargetControl();
+                targetControl.SetTitle(title);
+                targetControl.SetDescription(description);
+                targetControl.SetTimesCompleted(timesCompleted);
+                targetControl.SetToday(targetToday);
+                targetControl.SetGoalID(goalId);
+
+                //Add target to target area
+                targetArea.Children.Add(targetControl);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
